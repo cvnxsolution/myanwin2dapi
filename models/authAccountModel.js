241546129c4e7cbd0d6507d0fb56415f745e6286
zinminthet <cvnxsolution@gmail.com>
2025-05-21 21:58:05 +0630

    feat(auth): add password hashing and check password method

--- models/authAccountModel.js
+++ models/authAccountModel.js
@@ -1,27 +1,45 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const authAccountSchema = new mongoose.Schema({
-  role: {
-    type: String,
-    enum: ["user", "agent", "admin"],
-    required: [true, "role must be user, agent or admin"],
+const authAccountSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      enum: ["user", "agent", "admin"],
+      required: [true, "role must be user, agent or admin"],
+    },
+    refID: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: [true, "refID is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      select: false,
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "confirm password is required"],
+    },
   },
-  refID: {
-    type: mongoose.SchemaTypes.ObjectId,
-    required: [true, "refID is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "email is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
-  },
-  confirmPassword: {
-    type: String,
-    required: [true, "confirm password is required"],
-  },
+  { timestamps: true }
+);
+
+authAccountSchema.methods.isCorrectPassword = async function (
+  userPassword,
+  hashedPassword
+) {
+  return await bcrypt.compare(userPassword, hashedPassword);
+};
+
+authAccountSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
 const AuthAccount = mongoose.model("AuthAccount", authAccountSchema);
