a3dd057f754966e077054d3f42c76f779ece12cd
zinminthet <cvnxsolution@gmail.com>
2025-05-21 22:02:22 +0630

    fix(bug): fixed password rehashing each update

--- models/authAccountModel.js
+++ models/authAccountModel.js
@@ -37,6 +37,7 @@ authAccountSchema.methods.isCorrectPassword = async function (
 };
 
 authAccountSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
   this.confirmPassword = undefined;
   next();
