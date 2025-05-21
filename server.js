4954edc19c74878bee58baf5bfd4e2fcb123cdd1
zinminthet <cvnxsolution@gmail.com>
2025-05-21 17:30:38 +0630

    chore(server): initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/myanwin2d").then(() => {
+  console.log("db connected");
+});
+
+app.listen(8000, (error) => {
+  console.log("server is listening on port 8000");
+});
