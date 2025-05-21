cf1597230fd087570e1eeaa3a2e7a9d5baa46f52
zinminthet <cvnxsolution@gmail.com>
2025-05-21 23:14:45 +0630

    config(utils): extend jwt token versioning

--- utils/jwtUtil.js
+++ utils/jwtUtil.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 
-exports.signJWTToken = (id) => {
+exports.signJWTToken = (id, tokenVersion = 0) => {
   return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
