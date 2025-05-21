d8d68a04673daacf5236770385a5d5b19f2a9308
zinminthet <cvnxsolution@gmail.com>
2025-05-21 17:19:26 +0630

    feat(app): inital app placeholder

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const globalErrorHandler = require("./controllers/errorController");
+const authRoute = require("./routes/authRoute");
+const CustomAppError = require("./utils/CustomAppError");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/v1/auth", authRoute);
+
+app.use(globalErrorHandler);
+
+module.exports = app;
