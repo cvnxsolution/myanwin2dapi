a53f5d8cf7b1cc05caf8987bea76707cca496af2
zinminthet <cvnxsolution@gmail.com>
2025-05-21 17:27:49 +0630

    chore(model): add blueprint user model

---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+  },
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
