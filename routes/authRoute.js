a72935f67cd80c04a0339d1d8ca8e2db5f316609
zinminthet <cvnxsolution@gmail.com>
2025-05-23 16:46:45 +0630

    feat(route): add / route to agent

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,4 +7,6 @@ const authController = require("../controllers/authController");
 router.route("/login").post(limiter.loginLimiter, authController.login);
 router.route("/signup").post(authController.signUp);
 
+router.route("/").get(authController.getAllAuthAccounts);
+
 module.exports = router;
