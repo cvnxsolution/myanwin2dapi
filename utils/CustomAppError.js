1eb726e09e2de06679520047a04c4a7cf66075ae
zinminthet <cvnxsolution@gmail.com>
2025-05-23 16:47:09 +0630

    add statusCode fallback 500

--- utils/CustomAppError.js
+++ utils/CustomAppError.js
@@ -1,7 +1,7 @@
 class CustomAppError extends Error {
   constructor(message, statusCode) {
     super(message);
-    this.statusCode = statusCode;
+    this.statusCode = statusCode || 500;
     this.status = `${statusCode}`.startsWith("4") ? "error" : "fail";
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
