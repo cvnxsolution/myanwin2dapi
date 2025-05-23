6fcf962077b2020a1f6a84ade122eab7ebc6cc97
zinminthet <cvnxsolution@gmail.com>
2025-05-23 16:47:47 +0630

    fix naming and add account id

--- utils/jwtUtil.js
+++ utils/jwtUtil.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 
-exports.signJWTToken = (id, role, tokenVersion = 0) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET_KEY, {
+exports.signJWTToken = (auth_id, role, tokenVersion = 0, user_id) => {
+  return jwt.sign({ auth_id, role, user_id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
