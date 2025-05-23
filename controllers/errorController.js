31803d99955f94ee3305c7b6f0a65bf616c42e85
zinminthet <cvnxsolution@gmail.com>
2025-05-23 12:01:02 +0630

    feat(sad path): separate error response for proudction and developement and handle jwt error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,15 +1,44 @@
-const globalErrorHandler = (err, req, res) => {
+const CustomAppError = require("../utils/CustomAppError");
+
+const handleJWTError = (err) => {
+  return new CustomAppError(err.message, 400);
+};
+
+const sendErrorInDevelopment = (error, res) => {
+  return res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErorrInProduction = (error, res) => {
+  return res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+  });
+};
+
+const globalErrorHandler = (err, req, res, next) => {
   if (err.isOperational) {
     return res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
       error: err,
     });
-  } else {
-    return res.status(500).json({
-      status: "fail",
-      erorr: err,
-    });
+  }
+  let error = {};
+  switch (err.name) {
+    case "JsonWebTokenError":
+      error = handleJWTError(err);
+      break;
+  }
+  error = { ...error, stack: err.stack, message: err.message };
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorInDevelopment(error, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErorrInProduction(error, res);
   }
 };
 
