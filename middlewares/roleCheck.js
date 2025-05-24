8bac64c16d11424a167065fb5ea867ce3bc7085d
zinminthet <cvnxsolution@gmail.com>
2025-05-24 21:23:50 +0630

    sec(auth): restrcit only admin can access authAccounts

--- middlewares/roleCheck.js
+++
@@ -1,3 +0,0 @@
-exports.isAdmin = (req, res, next) => {
-    
-};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 const limiter = require("../middlewares/limiter");
 
+const { isAuthenticated } = require("../middlewares/isAuthenticated");
 const authController = require("../controllers/authController");
+const { restrictTo } = require("../middlewares/restrictTo");
 
 router.route("/login").post(limiter.loginLimiter, authController.login);
-router.route("/signup").post(authController.signUp);
+router.route("/signup").post(limiter.signUpLimiter, authController.signUp);
 
-router.route("/").get(authController.getAllAuthAccounts);
+router.use(isAuthenticated);
+
+router
+  .route("/")
+  .get(
+    restrictTo("Only admins are allowed", "admin"),
+    authController.getAllAuthAccounts
+  );
 
 module.exports = router;
