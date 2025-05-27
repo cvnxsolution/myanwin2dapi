53a1dab99cf2623cbef631fd072a87ffefc50a05
zinminthet <cvnxsolution@gmail.com>
2025-05-27 23:35:25 +0630

    add config for 2d draw time, soon to be in DB

---
+++ config/cutoffConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "2D": {
+    afternoon: "T05:31:00.000Z", // 12:01 PM MMT
+    evening: "T10:00:00.000Z", // 4:30 PM MMT
+  },
+
+  //   can extends for 3d here for next version update (cvnxsolution)
+};
