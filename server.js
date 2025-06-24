52f835a5e5d750b6a96c6ca577bac28fd9a6e9db
zinminthet <cvnxsolution@gmail.com>
2025-06-24 13:52:38 +0630

    fix(naming): rename certain variable

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const Database = process.env.Database;
+const LocalDb = process.env.LocalDB;
 
-mongoose.connect(Database).then(() => {
-  console.log("db connected");
-});
+const environment = process.env.NODE_ENV;
+
+if (environment === "development") {
+  mongoose.connect(LocalDb).then(() => {
+    console.log("local db connected");
+  });
+} else {
+  mongoose.connect(Database).then(() => {
+    console.log("mongodb atlas connected");
+  });
+}
 
 app.listen(8000, (error) => {
   console.log("server is listening on port 8000");
